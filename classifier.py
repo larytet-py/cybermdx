3bb9fab15023b3457a98e1d1bfcb8170a42aad81
arkady <larytet@yahoo.com>
2021-02-17 16:21:55 +0200

    protocol name

--- classifier.py
+++ classifier.py
@@ -4,16 +4,59 @@ Produce classifications for every line in the communication file
 '''
 
 from collections import namedtuple
+import netaddr
+
+
+def subnet_match(ip_address, subnet):
+    '''
+    https://stackoverflow.com/questions/819355/how-can-i-check-if-an-ip-is-in-a-network-in-python
+    '''
+    return netaddr.IPAddress(ip_address) in netaddr.IPNetwork(subnet)
 
 Communication = namedtuple('Communication', ["id", "timestamp", "device_id", "protocol_name", "host"])
 
 class RuleCommunicatingProtocol():
-    def __init__(self, id, protocol_name):
+    def __init__(self, id, protocol_name, classification):
         self.id, self.protocol_name = id, protocol_name
+        self.classification = classification
         
+    def type():
+        return "communicating_protocol"
+
     def match(communication):
-        return communication.protocol_name == self.protocol_name
+        if communication.protocol_name == self.protocol_name:
+            return self.classification
         
+
+class RuleCommunicatingWith():
+    '''
+    IPv4 only
+    '''
+    def __init__(self, id, ip_address, classification):
+        self.id, self.ip_address = id, ip_address
+        self.classification = classification
+        
+    def type():
+        return "communicating_with"
+
+    def match(communication):
+        if communication.ip_address == self.ip_address:
+            return self.classification
+
+class RuleCommunicatingWithSubnet():
+    '''
+    IPv4 only
+    '''
+    def __init__(self, id, subnet, classification):
+        self.id, self.ip_address = id, ip_address
+        self.classification = classification
+        
+    def type():
+        return "communicating_with"
+
+    def match(communication):
+        if communication.ip_address == self.ip_address:
+            return self.classification
 
 
 def loadRules():
